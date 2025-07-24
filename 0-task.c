57fe536d3fd06f3a6b0c3f55122a56592819e14a
andreamgrs <andrea.munozgr@gmail.com>
2025-07-24 02:28:57 +0000

    Adding new printf.c

--- 0-task.c
+++ 0-task.c
@@ -1,21 +1,38 @@
+#include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <limits.h>
 
+/**
+ * print_char - Printing char
+ *
+ * @arg: something
+ *
+ * Return -
+ */
 
 int print_char(va_list arg)
 {
 	char c;
+
 	c = va_arg(arg, int);
 	write(1, &c, 1);
 	return (1);
 }
 
+/**
+ * print_string - Printing string
+ *
+ * @arg: something
+ *
+ * Return -
+ */
+
 int print_string(va_list arg)
 {
 	char *s;
 	int len_string;
+
 	s = va_arg(arg, char *);
 	if (s == NULL)
 	{
@@ -24,34 +41,60 @@ int print_string(va_list arg)
 	len_string = 0;
 	while (s[len_string] != '\0')
 	{
-		len_string++;
+		len_string = len_string + 1;
 	}
 	write(1, s, len_string);
 	return (len_string);
 }
+
+/**
+ * print_int - Print decimal integer
+ *
+ * @arg: something
+ *
+ * Return -
+
+
+void print_int(va_list arg)
+{
+	int atoi(char d);
+	d = va_arg(arg, int);
+	write(1, &c, 1);
+}*/
+/**
+ * _printf - SOmething something
+ *
+ * @format: something
+ *
+ * Return -
+ */
 
 int _printf(const char *format, ...)
 {
 	int countformat = 0;
 	int counting_list = 0;
 	int printed_count = 0;
+	int matched;
 	va_list args;
 	op_list ops[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{NULL, NULL}
 	};
+
 	if (format == NULL)
 	{
 		return (-1);
 	}
+
 	va_start(args, format);
+
 	while (format[countformat] != '\0')
 	{
 		if (format[countformat] == '%')
 		{
 			counting_list = 0;
-			int matched = 0;
+			matched = 0;
 
 			while (ops[counting_list].op != NULL)
 			{
@@ -75,7 +118,7 @@ int _printf(const char *format, ...)
 		write(1, &format[countformat], 1);
 		printed_count++;
 		countformat++;
-		}
+	}
 	va_end(args);
 	return (printed_count);
 }
